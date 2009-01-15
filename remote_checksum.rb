f655d9fb038a65e61c4d457337bebe8119a7fcd8
Chad Woolley <thewoolleyman@gmail.com>
2009-01-14 18:45:58 -0700

    caw/dxue - second pass

--- remote_checksum.rb
+++ remote_checksum.rb
@@ -1,4 +1,4 @@
-class CheckRemoteChecksum < Scout::Plugin
+class RemoteChecksum < Scout::Plugin
   def build_report
     begin
       remote_host = option(:remote_host)
@@ -8,11 +8,11 @@ class CheckRemoteChecksum < Scout::Plugin
       output = "Remote Host: #{remote_host}, Remote MD5 Path: #{remote_md5_path}, MD5 output: #{output}, exit code: #{exitcode}"
       report(:md5_exitcode => exitcode)
       if exitcode != 0
-        alert(:subject => "Checksum failed for file #{remote_md5_path}", :body => output)
+        alert(:subject => "Remote Checksum failed for file #{remote_md5_path}", :body => output)
       end
       return exitcode
     rescue Exception => e
-      error(:subject => 'Error running Check Remote Checksum plugin', :body => e)
+      error(:subject => 'Error running Remote Checksum plugin', :body => e)
       return -1
     end
   end
