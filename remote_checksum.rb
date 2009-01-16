be05695cf284efbcbd3a3a91b468f87cdc20df01
pivotal <pivotal@alvarado.flood.pivotallabs.com>
2009-01-16 15:42:44 -0800

    dx/thewoolleyman - hackin' without tests cuz that's how we roll

--- remote_checksum.rb
+++ remote_checksum.rb
@@ -7,18 +7,16 @@ class RemoteChecksum < Scout::Plugin
       output = `ssh #{remote_host} 'md5sum -c #{remote_md5_path}'`
       exitcode = $?.exitstatus.to_i
       output = "Remote Host: #{remote_host}, Remote MD5 Path: #{remote_md5_path}, MD5 output: #{output}, exit code: #{exitcode}"
-      #if exitcode != 0
-       # alert(:subject => "Remote Checksum failed for file #{remote_md5_path}", :body => output)
-      #elsif
+      if exitcode != 0
+       alert(:subject => "Remote Checksum failed for file #{remote_md5_path}", :body => output)
+      elsif
         ls_output = `ssh #{remote_host} 'ls -l --time-style=long-iso #{remote_md5_path}'`
-        alert(:subject => "Debug 1", :body => ls_output)
         md5_file_dates = ls_output.split("\n").map {|line| datestring = line.split(' ')[5]; Date.strptime(datestring,"%Y-%m-%d") }
-        alert(:subject => "Debug 2", :body => md5_file_dates.first.to_s)
         unless md5_file_dates.all? {|date| date > Date.today - file_age_threshold_days}
           alert(:subject => "Remote MD5 Path '#{remote_md5_path}' older than threshold of #{file_age_threshold_days}", :body => ls_output)
           exitcode = 2
         end
-      # end
+      end
       report(:exit_code => exitcode)
       return exitcode
     rescue Exception => e
