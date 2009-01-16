7d6def7b254c67dd53e1109f569e345bd3f2ffdc
pivotal <pivotal@alvarado.flood.pivotallabs.com>
2009-01-16 15:29:58 -0800

    dx/thewoolleyman - hackin' without tests cuz that's how we roll

--- remote_checksum.rb
+++ remote_checksum.rb
@@ -10,16 +10,16 @@ class RemoteChecksum < Scout::Plugin
       #if exitcode != 0
        # alert(:subject => "Remote Checksum failed for file #{remote_md5_path}", :body => output)
       #elsif
-        ls_output = `ssh #{remote_host} 'ls -lTt #{remote_md5_path}'`
+        ls_output = `ssh #{remote_host} 'ls -l --time-style=long-iso #{remote_md5_path}'`
         alert(:subject => "Debug 1", :body => ls_output)
         md5_file_dates = ls_output.split("\n").map {|line| datestring = line.split(' ')[5]; Date.strptime(datestring,"%Y-%m-%d") }
         alert(:subject => "Debug 2", :body => md5_file_dates.first.to_s)
         unless md5_file_dates.all? {|date| date > Date.today - file_age_threshold_days}
           alert(:subject => "Remote MD5 Path '#{remote_md5_path}' older than threshold of #{file_age_threshold_days}", :body => ls_output)
-          exitcode = 1
+          exitcode = 2
         end
       # end
-      report(:md5_exitcode => exitcode)
+      report(:exit_code => exitcode)
       return exitcode
     rescue Exception => e
       error(:subject => 'Error running Remote Checksum plugin', :body => e)
